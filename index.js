3b1f3c2eb6b18a8c984390206a38ec388f309fb0
SaatwikRishi <38259107+SaatwikRishi@users.noreply.github.com>
2022-07-11 13:20:46 +0530

    f

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post("/challenge", (req, res) => {
     const ans = {
       is_success: true,
       user_id: "utkarsh_26072000",
-      count: toString(alphaNumericCount),
+      count: (alphaNumericCount.toString()),
       numbers: numCount,
       alphabets: letterCount,
       roll_number: "1906449",
